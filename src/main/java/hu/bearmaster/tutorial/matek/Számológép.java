63e55488cf667b726c8b71bd3cb7d7982d23135a
javaprogramozas <javaprogramozas.hu@gmail.com>
2022-01-09 14:43:49 +0100

    M1 - Legkisebb közös többszörös

--- src/main/java/hu/bearmaster/tutorial/matek/Számológép.java
+++ src/main/java/hu/bearmaster/tutorial/matek/Számológép.java
@@ -1,5 +1,10 @@
 package hu.bearmaster.tutorial.matek;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Számológép {
 
     private final PrímTár prímTár;
@@ -21,10 +26,8 @@ public class Számológép {
         int prím = 2;
         while (szám > 1) {
             if (szám % prím == 0) {
-                PrímTényező újTényező = felbontás.getPrímTényező(prím)
-                        .map(tényező -> new PrímTényező(tényező.getPrím(), tényező.getKitevő() + 1))
-                        .orElse(new PrímTényező(prím, 1));
-                felbontás.setPrímTényező(újTényező);
+                int tényező = felbontás.getPrímTényező(prím);
+                felbontás.setPrímTényező(prím, tényező + 1);
                 szám = szám / prím;
             } else {
                 prím = prímTár.következőPrím(prím);
@@ -32,6 +35,36 @@ public class Számológép {
         }
 
         return felbontás;
+    }
+
+    public int lkkt(List<Integer> számok) {
+        if (számok.size() < 2) {
+            return -1;
+        }
+        List<PrímTényezősFelbontás> felbontások = new ArrayList<>();
+        for (int szám : számok) {
+            PrímTényezősFelbontás felbontás = prímFelbontás(szám);
+            felbontások.add(felbontás);
+        }
+
+        Set<Integer> összesPrímOsztók = new HashSet<>();
+
+        for (PrímTényezősFelbontás felbontás : felbontások) {
+            összesPrímOsztók.addAll(felbontás.prímek());
+        }
+
+        PrímTényezősFelbontás lkktFelbontás = new PrímTényezősFelbontás();
+
+        for (int prím : összesPrímOsztók) {
+            int kitevő = 0;
+            for (PrímTényezősFelbontás felbontás : felbontások) {
+                int aktuálisKitevő = felbontás.getPrímTényező(prím);
+                kitevő = aktuálisKitevő > kitevő ? aktuálisKitevő : kitevő;
+            }
+            lkktFelbontás.setPrímTényező(prím, kitevő);
+        }
+
+        return lkktFelbontás.érték();
     }
 
     public String státusz() {
